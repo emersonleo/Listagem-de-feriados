374bc5e81898b554bdcb829b1ea2073bca7f7b94
emersonleo <emerson.leonardo@ufrpe.br>
2020-04-25 18:21:24 -0300

    Adição do modal de listagem dos feriados de acordo com a cidade

--- application/views/main.php
+++ application/views/main.php
@@ -3,6 +3,10 @@
 <html>
 <head>
 	<title> Página principal </title>
+	<script src="https://code.jquery.com/jquery-3.5.0.js" integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc=" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+	 integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
 	<h1> 
@@ -15,11 +19,38 @@
 	<select id='selectCidade'> 
 		<option id="9999" selected  disabled> Selecione uma cidade </option>
 	</select>
-	<button id="btnGerar"> Gerar </button>
+	<button id="btnGerar" data-toggle="modal" data-target="#modalFeriado"> Gerar </button>
+
+	<div class="modal" id="modalFeriado" tabindex="-1" role="dialog" >
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title"> Feriados </h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        <table id='tableModal'>
+	        	<thead>
+	        		
+	        	</thead>
+	        	<tbody id='tbodyModal'>
+	        		
+	        	</tbody>
+	        </table>
+	      </div>
+	      <div class="modal-footer">
+	      	<button > Baixar planilha </button>
+	      </div>
+
+	    </div>
+	  </div>
+	</div>
 </body>
 </html>
 
-<script src="https://code.jquery.com/jquery-3.5.0.js" integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc=" crossorigin="anonymous"></script>
+
 <script type="text/javascript">
 $.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados', function(data){
 	var tamanhoJson = Object.keys(data).length
@@ -33,6 +64,7 @@ $.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados', function(da
 	$('#selectEstado')[0].innerHTML += selectBody;
 })
 $('#selectEstado').change(function(data){
+
 	$('#selectCidade')[0].innerHTML = '<option id="9999" selected  disabled> Selecione uma cidade </option>'
 	var select = $('#selectEstado')[0]
 	var idEstado = select.options[select.selectedIndex].id
@@ -51,15 +83,20 @@ $('#btnGerar').click(function(data){
 	var select = $('#selectCidade')[0]
 	var idCidade = select.options[select.selectedIndex].id
 	if(idCidade == '9999'){
-		console.log(idCidade)
 		return
 	}
 	var ano = new Date().getFullYear()
-	console.log(idCidade)
-	$.get('https://api.calendario.com.br/?ano='+ ano + '&ibge='+ idCidade +'&token=YmFvLmVtZXJzb25AZ21haWwuY29tJmhhc2g9NjQxMzY1OTI&json=true',function(data){
-		
+	$.get('https://api.calendario.com.br/?ano='+ ano + '&ibge='+ idCidade +'&token=YmFvZW1lcnNvbjE1QGdtYWlsLmNvbSZoYXNoPTEzNjcxNzIxMA&json=true',function(dataFeriado){
+		console.log(dataFeriado)
+		var tamanhoJson = Object.keys(dataFeriado).length
+		var strTbody = '';
+		for(var i= 0; i < tamanhoJson; i++){
+			strTbody += '<tr align="center"><td style="width:30%">' +  dataFeriado[i].date + '</td> <td style="width:50%">' + dataFeriado[i].name + '</td></tr>'
+		}
+		$("#tbodyModal")[0].innerHTML = strTbody
 	})
 
 })
+
 
 </script>
